fdaac174684eabec1eeee95157c4b7143e4f2450
ROMKA9915 <romka9915@gmail.com>
2025-04-02 19:55:13 +0300

    AN 2.4 CRUD

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,16 +2,20 @@ package ru.netology.nmedia
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.Group
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.databinding.CardPostBinding
-import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostViewModel
+import ru.netology.nmedia.R
+import ru.netology.nmedia.adapter.OnInteractionListener
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.util.AndroidUtils
 import java.math.RoundingMode
 
 class MainActivity : AppCompatActivity() {
@@ -27,19 +31,69 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val adapter = PostsAdapter({
-            viewModel.likeById(it.id)
-        }, {viewModel.shareById(it.id)})
+        val adapter = PostsAdapter(object : OnInteractionListener {
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onShare(post: Post) {
+                viewModel.shareById(post.id)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun onEdit(post: Post) {
+                viewModel.edit(post)
+                binding.editedPost.text = post.content
+                binding.editGroup.visibility = Group.VISIBLE
+            }
+        })
 
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
-            adapter.submitList(posts)
-
-                    }
+            val newPost = adapter.currentList.size < posts.size
+            adapter.submitList(posts) {
+                if (newPost) {
+                    binding.list.scrollToPosition(0)
                 }
             }
+        }
+
+        viewModel.edited.observe(this) {
+            if (it.id != 0L) {
+                binding.content.setText(it.content)
+                binding.content.requestFocus()
+            }
+        }
+
+        binding.add.setOnClickListener {
+            val text = binding.content.text.toString()
+            if (text.isBlank()) {
+                Toast.makeText(this, R.string.error_empty_content, Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            viewModel.changeContentAndSave(text)
+
+                binding.content.setText("")
+            binding.editGroup.visibility = Group.GONE
+            binding.content.clearFocus()
+            AndroidUtils.hideKeyboard(it)
+        }
+
+        binding.cancelEdit.setOnClickListener {
+            viewModel.clearEdit()
+            binding.editGroup.visibility = Group.GONE
+            binding.content.setText("")
+            binding.content.clearFocus()
+            AndroidUtils.hideKeyboard(it)
+        }
+    }
+}
 
 @SuppressLint("DefaultLocale")
 fun scaleNumbers(number: String): String {
--- app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
+++ app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
@@ -2,6 +2,7 @@ package ru.netology.nmedia.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -10,15 +11,19 @@ import ru.netology.nmedia.databinding.CardPostBinding
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.scaleNumbers
 
-typealias OnLikeListener = (Post) -> Unit
-typealias OnShareListener = (Post) -> Unit
+interface OnInteractionListener {
+    fun onLike (post: Post)
+    fun onShare (post: Post)
+    fun onRemove (post: Post)
+    fun onEdit (post: Post)
+}
 
-class PostsAdapter(private val onLikeListener: OnLikeListener, private val onShareListener: OnShareListener) :
+class PostsAdapter(private val onInteractionListener: OnInteractionListener) :
     ListAdapter<Post, PostViewHolder>(PostDiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
         val view = CardPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return PostViewHolder(view, onLikeListener, onShareListener)
+        return PostViewHolder(view, onInteractionListener)
     }
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
@@ -28,8 +33,7 @@ class PostsAdapter(private val onLikeListener: OnLikeListener, private val onSha
 
 class PostViewHolder(
     private val binding: CardPostBinding,
-    private val onLikeListener: OnLikeListener,
-    private val onShareListener: OnShareListener
+    private val onInteractionListener: OnInteractionListener
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(post: Post) = with(binding) {
@@ -45,11 +49,30 @@ class PostViewHolder(
             if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24
         )
         likes.setOnClickListener {
-            onLikeListener(post)
+            onInteractionListener.onLike(post)
         }
 
         shares.setOnClickListener {
-            onShareListener(post)
+            onInteractionListener.onShare(post)
+        }
+
+        menu.setOnClickListener {
+            PopupMenu(it.context, it).apply {
+                inflate(R.menu.post_actions)
+                setOnMenuItemClickListener { item ->
+                    when (item.itemId) {
+                        R.id.remove -> {
+                            onInteractionListener.onRemove(post)
+                            true
+                        }
+                        R.id.edit -> {
+                            onInteractionListener.onEdit(post)
+                            true
+                        }
+                        else -> false
+                    }
+                }
+            }.show()
         }
     }
 }
